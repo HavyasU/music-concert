c67c83c9414838110b957f8f5b13c1b62a020616
havyasu <havyasu2005@gmail.com>
2025-11-25 23:21:50 +0530

    commit

--- backend/controllers/homeController.js
+++
@@ -1,6 +0,0 @@
-export const sayHello = (req, res) => {
-    res.send("Hello!");
-};
-export const sayBye = (req, res) => {
-    res.send("Hello!");
-};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { homeRouter } from "./routes/homeRoutes.js";
 
 const server = express();
 const port = 8000;
 
 
-server.use('/', homeRouter);
 
 const serverListen = () => {
     console.log("Server is Runnning!");
